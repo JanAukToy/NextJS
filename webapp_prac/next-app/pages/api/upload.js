7f0dc487468c1fdcfbc86de4318cfc0263580f07
JanAukToy <fxhnx731@ybb.ne.jp>
2021-09-08 23:53:53 +0900

    master

--- webapp_prac/next-app/pages/api/upload.js
+++ webapp_prac/next-app/pages/api/upload.js
@@ -1,8 +1,59 @@
+import nextConnect from 'next-connect';
+import multer from 'multer';
 
+// multer処理（オプション設定）
+const upload = multer({
+    storage: multer.diskStorage({
+        // 保存先
+        destination: './public/uploads',
+        // ファイル名
+        filename: (req, file, cb) => cb(null, Get_FileName(file.originalname)),
+    }),
+});
 
+// nextConnect取得、エラーファンクション設定
+const apiRoute = nextConnect({
+    onError(error, req, res) {
+        res.status(501).json({ error: `${error.message}` });
+    },
+    onNoMatch(req, res) {
+        res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+    },
+});
 
-export default function handler(req, res) {
-    if (method === "GET"){
-        res.json({ status: 'success' });
-    }
-  }
+// multerファイル保存をミドルウェアとして指定（Field:fileのみ受付）
+apiRoute.use((req, res, next) => {
+    // メソッドタイプと保存するファイルか確認
+    if ((req.method === "POST") && (is_Correctfile(req.file.originalname))){
+        // multer処理へ（ファイル保存）
+        next();
+    }    
+}, upload.single('file')
+);
+
+// レスポンス
+apiRoute.post((req, res) => {
+    res.status(200).json({status: 'success!'});
+});
+
+// ミドルウェア指定のコンポーネントとしてセット
+export default apiRoute;
+
+// API設定（https://nextjs-ja-translation-docs.vercel.app/docs/api-routes/api-middlewares）
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
+
+// 保存するか否か判断
+function is_Correctfile(original_name){
+    // テストのため全スルー
+    return true;
+}
+
+// 保存名変更ハンドラ
+function Get_FileName(original_name){
+    // テストのため全スルー
+    return original_name;
+}
